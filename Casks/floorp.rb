3508c9240f77abf03d978e35cf021140dbe60581
AraigumaG <araigumaG@users.noreply.github.com>
2022-10-22 23:01:18 +0900

    Floorp 10.6.2
    Update Floorp from 10.6.1 to 10.6.2

--- Casks/floorp.rb
+++ Casks/floorp.rb
@@ -1,6 +1,6 @@
 cask "floorp" do
-  version "10.6.1"
-  sha256 "6c1bc467c9431c0c31f6dec0fcf34ddc312a09cf7369d358d4e8ae3e3769cf54"
+  version "10.6.2"
+  sha256 "fa6a90d7b62e0e2fd9554c5ca91ae340b2f172accff4e27f299dc49a455b10db"
 
   url "https://github.com/Floorp-Projects/Floorp/releases/download/v#{version}/floorp-macOS-Univerasal.dmg",
   verified: "github.com/Floorp-Projects"
