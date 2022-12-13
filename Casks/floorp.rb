a305468fd2ac43dd5596150b6524b0cb91e2f399
AraigumaG <araigumaG@users.noreply.github.com>
2022-12-13 19:27:11 +0900

    Floorp 10.8.0
    Update Floorp from 10.7.0 to 10.8.0

--- Casks/floorp.rb
+++ Casks/floorp.rb
@@ -1,6 +1,6 @@
 cask "floorp" do
-  version "10.7.0"
-  sha256 "3e0fb9bab98e92e81bfe9c6ae454b2fee7f5c08b1ac5dcf5856a29b8366a1573"
+  version "10.8.0"
+  sha256 "48fe75ec4bb8ff9366219cd36696023b58bd12f982f56d400906c55282902977"
 
   url "https://github.com/Floorp-Projects/Floorp/releases/download/v#{version}/floorp-macOS-universal.dmg",
   verified: "github.com/Floorp-Projects"
